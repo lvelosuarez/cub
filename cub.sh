f135a8717e4127b3fdbd1150f2f7f33d22d369e9
Lourdes <lourdesvelo@gmail.com>
2022-02-17 15:57:40 +0100

    modif sacha for cloudgene

--- cub.sh
+++ cub.sh
@@ -1,13 +1,21 @@
 #!/bin/zsh
 
+source /home/lvelo/miniconda3/etc/profile.d/conda.sh 
+
+
 # This file is part of the cub taxonomic sequence classification system.
 # Runs the whole bash and snakemake pipeline.  
 
-run="$1"
-nas="/mnt/R60-11/Bacterio/metagenomique_NextSeq/"
+RUN_NAME=$1
+NAS="/mnt/R60-11/Bacterio/metagenomique_NextSeq/"
+
+RUN_FOLDER=${NAS}/${RUN_NAME}
+
+PROJECT_PATH="/DATA/share/microbio/cub/"
+
 # activate cub environment
 conda activate cub
-# run the pipeline
- ./create_table.py $run
-#snakemake -nfp -d $run
-snakemake --use-conda -j 2 -d $run
+# RUN_FOLDER the pipeline
+python $PROJECT_PATH/create_table.py $RUN_FOLDER
+snakemake -nfp -d $RUN_FOLDER -s $PROJECT_PATH/Snakefile
+#snakemake --use-conda -j 2 -d $RUN_FOLDER
