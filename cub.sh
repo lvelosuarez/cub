7940bd6c8a62844c878e310ead2ede5ff0f0ea7c
0143813A <lourdes.velosuarez@chu-brest.fr>
2024-06-11 14:01:38 +0000

    Update file cub.sh

--- cub.sh
+++ cub.sh
@@ -1,22 +1,28 @@
 #!/bin/zsh
 
-source /home/lvelo/miniconda3/etc/profile.d/conda.sh 
+# proton path 
+# source /home/lvelo/miniconda3/etc/profile.d/conda.sh 
 
+# fermion path
+source /data/lourdes/miniconda3/etc/profile.d/conda.sh
 
 # This file is part of the cub taxonomic sequence classification system.
 # Runs the whole bash and snakemake pipeline.  
 
 RUN_NAME=$1
-NAS="/mnt/R60-11/Bacterio/metagenomique_NextSeq"
-
+# This is the real NAS but I will work sometimes in run located in local NAS
+#NAS="/mnt/R60-11/Bacterio/metagenomique_NextSeq"
+NAS="/data/lourdes/NAS"
 RUN_FOLDER=${NAS}/${RUN_NAME}
 
-PROJECT_PATH="/DATA/share/microbio/cub"
-
+# proton path 
+#PROJECT_PATH="/DATA/share/microbio/cub"
+# fermion path 
+PROJECT_PATH="/data/lourdes/metagenomique_clinique"
 # activate cub environment
-conda activate cub_3_8
+conda activate cub # fermion environment
 # RUN_FOLDER the pipeline
 echo $RUN_FOLDER
-python $PROJECT_PATH/create_table.py $RUN_FOLDER
+#python $PROJECT_PATH/create_table.py $RUN_FOLDER
 #snakemake -nfp -d $RUN_FOLDER -s $PROJECT_PATH/Snakefile #--rerun-incomplete  
-snakemake -d $RUN_FOLDER -s $PROJECT_PATH/Snakefile -j 1
+#snakemake -d $RUN_FOLDER -s $PROJECT_PATH/Snakefile -j 1
