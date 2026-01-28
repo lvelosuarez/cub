b6beef281b6ba96a106ec20383a85ffd5e3534e2
lourdes <lourdes.velosuarez@chu-brest.fr>
2026-01-28 16:27:21 +0100

    organize

--- scr/create_table.py
+++ scr/create_table.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 import os
-import logging
-import pandas as pd
 from collections import defaultdict
 
-def get_sample_files(path:str):
+import pandas as pd
+
+
+def get_sample_files(path: str):
     """
     Extract samples from NGS illumina folder
     """
@@ -19,15 +20,16 @@ def get_sample_files(path:str):
                 sample_id = sample_id.replace("_R1", "").replace("_R2", "").replace("_001", "").replace(" ", "-")
                 fq_path = os.path.join(dir_names, fname)
                 if "_R1" in fname:
-                    samples[sample_id]['R1'] = fq_path
+                    samples[sample_id]["R1"] = fq_path
                 else:
-                    samples[sample_id]['R2'] = fq_path
-    samples= pd.DataFrame(samples).T
+                    samples[sample_id]["R2"] = fq_path
+    samples = pd.DataFrame(samples).T
     # samples.to_csv(os.path.abspath(path) + "/" + outfile,sep='\t')
 
     print(samples.to_string())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     get_sample_files(sys.argv[1])
